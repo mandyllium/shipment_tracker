495ea6dd787acdc171abaa1192bdaced93bd2ec4
Ahuja Mandeep Singh <13878-qpjg1605@users.noreply.gitlab.forge.orange-labs.fr>
2020-03-25 22:52:29 +0530

    Shipment ID changes

--- call_robot.py
+++ call_robot.py
@@ -3,6 +3,7 @@ import sys
 
 
 def call_rpa(shipment_id, file_name):
+    shipment_id=shipment_id[0]
     python_path = sys.executable
     robotFile_variable = "-v shipment_id:" + shipment_id
     print(python_path, '-m', 'robot', robotFile_variable, file_name)
