16e0ee809a024449ba1ea959c1f107731f624257
Ahuja Mandeep Singh <13878-qpjg1605@users.noreply.gitlab.forge.orange-labs.fr>
2020-03-25 22:40:38 +0530

    Exception removed

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import time
 from MYSQlWrapper import MySql_Wrapper
 
 def main_call(mysql_obj):
-    try:
-        mysql_obj.shipment_id()
+    #try:
+    mysql_obj.shipment_id()
 
-    except Exception:
-        print("Hit again after 120 seconds")
-        time.sleep(120)
-        main_call(mysql_obj)
+    #except Exception:
+    print("Hit again after 120 seconds")
+    time.sleep(120)
+    main_call(mysql_obj)
 
 if __name__ == '__main__':
     mysql = MySql_Wrapper()
