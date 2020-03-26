7293f1c24f78ee35be8f00981681fbe859a64321
Ahuja Mandeep Singh <13878-qpjg1605@users.noreply.gitlab.forge.orange-labs.fr>
2020-03-26 23:19:29 +0530

    connection check

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import time
 from MYSQlWrapper import MYSQlWrapper
 
 def main_call(mysql_obj):
-    #try:
-    mysql_obj.shipment_id()
+    try:
+        mysql_obj.shipment_id()
 
-    #except Exception:
-    print("Hit again after 120 seconds")
-    time.sleep(120)
-    main_call(mysql_obj)
+    except Exception:
+        print("Hit again after 120 seconds")
+        time.sleep(120)
+        del(mysql_obj)
+        mysql_obj = MYSQlWrapper()
+        main_call(mysql_obj)
+
+    finally:
+        print("Hit again after 3 seconds")
+        time.sleep(3)
+        del (mysql_obj)
+        mysql_obj = MYSQlWrapper()
+        main_call(mysql_obj)
+
 
 if __name__ == '__main__':
     mysql = MYSQlWrapper()
